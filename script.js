b9aa7a374dd4d6e40c678e71ca85f740dff8d14a
anacgms <acgomes43@gmail.com>
2022-07-09 19:22:10 -0300

    firstenglish

---
+++ script.js
@@ -0,0 +1,69 @@
+let comedy = [
+{
+    title: "Free guy",
+    diretor: "Shawn Levy",
+    year: 2021
+},
+{
+    title: "Sing 2",
+    diretor: "Garth Jennings",
+    year: 2022
+},
+{
+    title: "The croods 2",
+    diretor: "Joel Crawford",
+    year: 2021
+}
+]
+
+let fantasy = [
+{
+    title: "Doctor Strange",
+    diretor: "Sam Raimi",
+    year: 2022
+},
+{
+    title: "Venom",
+    diretor: "Andy Serkis",
+    year: 2021
+},
+{
+    title: "Dumbo",
+    diretor: "Tim Burton",
+    year: 2019
+}
+]
+
+let actiom = [
+    {
+        title: "Top Gun",
+        diretor: "Joseph Kosinski",
+        year: 2022
+    },
+    {
+        title: "Morbius",
+        diretor: "Daniel Espinosa",
+        year: 2022
+    },
+    {
+        title: "Resident Evil - welcome to Raccoon",
+        diretor: "Johannes Roberts",
+        year: 2021
+    }
+    ]
+    
+       
+var favoriteMovie = false
+if(favoriteMovie){
+    console.log("My favorite movie is Doctor Strange")
+} else{
+    console.log("Not Even 5 Stars Resident Evil - Welcome to Raccoon")
+}
+
+
+
+
+
+//Escolha uma variável e insira uma condição para ela, (condição de sua escolha), com a seguinte mensagem ao console: 
+//IF: "MY FAVORIT MOVIE IS..."
+//ELSE: "NOT EVEN 5 STARS..." 
